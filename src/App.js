59b0ad5bcf4f0c914335bc3556956bded917dad6
jong6598 <108744804+jong6598@users.noreply.github.com>
2022-10-15 23:58:48 +0900

    delete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import './App.css';
-import { ThemeProvider } from 'styled-components';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import theme from './styles/theme.jsx';
 import Login from "./pages/Auth/Login";
 import Signup from "./pages/Auth/Singup";
 import Todos from './pages/Todo/Todos';
@@ -9,17 +7,16 @@ import Todos from './pages/Todo/Todos';
 function App() {
   const accessToken = localStorage.getItem("accessToken");
 
+  console.log(accessToken)
 
 
   return (
     <div className="App">
-      <ThemeProvider theme={theme}>
         <Routes>
           <Route path="/" element={accessToken? <Navigate to="/todo"/> : <Login />} />
           <Route path="/signup" element={accessToken? <Navigate to="/todo"/> : <Signup />} />
           <Route path="/todo" element={<Todos/>} />
         </Routes>
-      </ThemeProvider>
     </div>
   );
 }
